fee5a9fa0042b455f9de10dfdc6a485877dea8af
lm000088 <LM000088@sd38.bc.ca>
2021-09-23 12:49:41 -0700

    ill change this later

--- mcevoy_lorraine_p1q1.py
+++ mcevoy_lorraine_p1q1.py
@@ -1,1 +1,16 @@
-print("yelling")
+print("""Here is a quiz about aquatic life!
+
+Question #1
+What is the largest animal in the ocean?
+A. Colossal Squid
+B. Blue Whale
+C. Whale Shark""")
+answer = input("Choose an option!")
+if answer == "b" or "B":
+    print("Correct!")
+elif "A" or "a" or "C" or "c":
+    print("Wrong")
+
+
+
+
