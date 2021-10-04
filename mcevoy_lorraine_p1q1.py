c086b5758acd384fad72add4aeed49150d5a1570
lm000088 <LM000088@sd38.bc.ca>
2021-10-04 12:50:12 -0700

    - added the rest of the questions - going to give them their total next time

--- mcevoy_lorraine_p1q1.py
+++ mcevoy_lorraine_p1q1.py
@@ -5,11 +5,57 @@ What is the largest animal in the ocean?
 A. Colossal Squid
 B. Blue Whale
 C. Whale Shark""")
-answer = input("Choose an option!")
-if answer == "b" or "B":
+answer = input("Choose an option! ")
+if answer == "b":
     print("Correct!")
-elif "A" or "a" or "C" or "c":
+elif answer == "B":
+    print("Correct!")
+else:
     print("Wrong")
+
+print("""
+Question #2
+How many hearts do octopus have??
+A. 3 hearts
+B. 6 hearts
+C. 9 hearts""")
+answer = input("Choose an option! ")
+if answer == "a":
+    print("Correct!")
+elif answer == "A":
+    print("Correct!")
+else:
+    print("Wrong")
+
+print("""
+Question #3
+How many brains do octopus have??
+A. 3 brains
+B. 6 brains
+C. 9 brains""")
+answer = input("Choose an option! ")
+if answer == "c":
+    print("Correct!")
+elif answer == "C":
+    print("Correct!")
+else:
+    print("Wrong")
+
+print("""
+Question #4
+What is the largest FISH in the sea?
+A. Whale Shark
+B. Blue Whale
+C. Giant Manta Ray""")
+answer = input("Choose an option! ")
+if answer == "a":
+    print("Correct!")
+elif answer == "A":
+    print("Correct!")
+else:
+    print("Wrong")
+
+
 
 
 
