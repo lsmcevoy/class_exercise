fb5db5f575928ad8ce9484853d4e79d78700ed35
lm000088 <LM000088@sd38.bc.ca>
2021-11-12 12:24:43 -0800

    - finished it

--- class.assignment.py
+++ class.assignment.py
@@ -1,73 +1,75 @@
 # classes_exercise.py
 
-"""
-1. Create a class according to the following requirements:
+"""1. Create a class according to the following requirements:
 It's name is Vehicle and it has the following attributes/methods:
+
 Attributes/properties:
     name: str
     max_speed: int
     capacity: int
-Methods:
+
+    Methods:
     vroom() -> None
         Prints "Vroom" max_speed times
-2. Create a child/subclass of Vehicle called Bus with the following methods:
+"""
+
+
+class Vehicle:
+    def __init__(self):
+        self.name = ""
+        self.max_speed = 0
+        self.capacity = 0
+
+    def vroom(self):
+        pass
+
+
+"""This a vehicle that has the following 
+      name: the vehicle name
+      max_speed: the vehicle's maximum speed
+      capacity: the amount of people the vehicle can carry"""
+
+
+def vroom(self) -> None:
+    print("Vroom" * self.max_speed)
+
+
+"""Vroom multiplied by the speed of the vehicle"""
+
+"""2. Create a child/subclass of Vehicle called Bus with the following methods:
 Methods:
     fare(age: float) -> None
         Prints "The fare of the bus ride is {}."
             Price depends on age:
                 0-17 years - Free
                 18-60 years - $5
-                61+ years - Free
-"""
-
-# Your code goes under here
-
-class Vehicle:
-    """Represents a vehicle
-    Attributes:
-        name: name of the vehicle
-        max_speed: maximum speed in km/h
-        capacity: how many people it can hold
-    """
-    def __init__(self):
-        self.name = ""
-        self.max_speed = 0
-        self.capacity = 0
-
-    def vroom(self) -> None:
-        """Vehicle goes vroom multipled by the
-        amount of speed times"""
-
-        print("Vroom" * self.max_speed)
+                61+ years - Free"""
 
 
 class Bus(Vehicle):
-    """Bus is a Vehicle that can drive
-    humans around in it"""
+    """This is a kind of vehicle called bus that can carry people"""
+
     def fare(self, age: int) -> None:
-        """Tells how much fare is for a particular age"""
+        """Given the age it will tell the fare."""
+
         if 18 <= age <= 60:
-            print("The fare of this bus ride is $5.00. 🚌")
+            print("The fare of this bus ride is $5.00")
+            """if someone is between the ages of 18 and 60, it will tell them the fare is $5"""
         else:
-            print("You ride free!~ 🚌")
+            print("Your ride is free.")
+            """if someone is under 18 or over 60, it will tell the their ride is free"""
 
 
-a_vehicle = Vehicle()
-a_vehicle.name = "La Ferrari"
-a_vehicle.max_speed = 372
-a_vehicle.capacity = 2
-a_vehicle.vroom()
+vehicle1 = Vehicle()
+vehicle1.name = "Car"
+vehicle1.max_speed = 100
+vehicle1.capacity = 4
+vehicle1.vroom()
 
-a_bus = Bus()
-a_bus.name = "Tranlink Bus - 407"
-a_bus.capacity = 35
-a_bus.max_speed = 140
-a_bus.vroom()
-a_bus.fare(10)
-print()
-a_bus.fare(-1)
-a_bus.fare(0)
-a_bus.fare(17)
-a_bus.fare(18)
-a_bus.fare(60)
-a_bus.fare(61)
+
+bus1 = Bus()
+bus1.name = 'A bus'
+bus1.max_speed = 100
+bus1.capacity = 40
+bus1.vroom()
+bus1.fare(20)
